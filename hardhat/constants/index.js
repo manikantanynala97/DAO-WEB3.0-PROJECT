c5fea3bd6c5aa6fd91396e6cdbc796c79030ea3a
manikantanynala97 <manikantanynala97@gmail.com>
2022-04-05 02:21:11 +0530

    mani commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xa8A3d4E72c61D75a7cdDcF0A4d8b9C5588c7E8a9";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
